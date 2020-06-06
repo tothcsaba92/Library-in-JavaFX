8a74509eb4e5f4e625e4bfdba6812af3a3d4ad21
Toth Csaba <toth.csaba.szeged@gmail.com>
2020-06-06 23:11:10 +0200

    css connected to fxml at listuser

--- src/library/Test/Test.java
+++ src/library/Test/Test.java
@@ -6,5 +6,6 @@ public class Test {
     public static void main(String[] args) {
         Console console = new Console(new BookDB());
         console.listUsers();
+        System.out.println(System.getProperties());
     }
 }
